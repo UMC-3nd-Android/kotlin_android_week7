33bac92100ffb5cf73e0c2db46475e349ebe0922
wbham97 <wbham97@naver.com>
2022-11-17 05:08:14 +0900

    doing well

--- app/src/main/java/com/example/week7/MainActivity.kt
+++ app/src/main/java/com/example/week7/MainActivity.kt
@@ -1,27 +1,73 @@
 package com.example.week7
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
+import android.media.SoundPool
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.example.week7.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
-        // 설정한 시간 지나면 화면에 타이머가 종료되었다고 알리면 됨.
-        // 상단바에서 띄우는 알림.(추가로 해보고 싶다면)
-        // 이거는 실습만으로도 ㄱㄴ.
-        // text 업데이트 할 때에 Handler를 써야한다는 것.
-        // 시간 흐르는 것은 타이머 내에서 해결
-        val min = 0
-        val sec = 0
-        Thread(){
-            while(true){
+        val soundPool = SoundPool.Builder().build()
+        val soundID = soundPool.load(this,R.raw.wtf,1)
 
-            }
-        }.start()
+        var isStarted: Boolean
+        val handler = Handler(mainLooper)
+
+        val myToast = Toast.makeText(this.applicationContext, "타이머 종료", Toast.LENGTH_SHORT)
+        val builder = AlertDialog.Builder(this)
+
+        viewBinding.btnStart.setOnClickListener {
+            val inMin = viewBinding.etMIn.text.toString()
+            val inSec = viewBinding.etSIn.text.toString()
+            var min = 0
+            var sec = 0
+
+            isStarted = true
+            Thread() {
+                while(isStarted){
+                    if(sec == 59){
+                        min++
+                        sec = 0
+                    }
+                    else{
+                        sec++
+                    }
+                    if(inMin == min.toString() && inSec == sec.toString()){
+                        isStarted = false
+                        handler.post {
+                            myToast.show()
+
+                            builder
+                                .setTitle("비상비상!!!")
+                                .setMessage("타이머 종료!!!!!@")
+                                .setPositiveButton("확인",
+                                    DialogInterface.OnClickListener { dialog, id ->
+                                        viewBinding.tvM.text = "00"
+                                        viewBinding.tvS.text = "00"
+                                    })
+                            builder.create()
+                            builder.show()
+
+                            soundPool.play(soundID,1.0f,1.0f,0,0,1.0f)
+                        }
+                    }
+                    handler.post {
+                        viewBinding.tvM.text = min.toString()
+                        viewBinding.tvS.text = sec.toString()
+                    }
+                    Thread.sleep(1000)
+                }
+            }.start()
+        }
     }
 }
